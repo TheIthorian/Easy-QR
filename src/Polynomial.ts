bc2905dcdc04017f3f9e2d5a8644d5cd44e526de
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-08-14 17:23:02 +0100

    Add types

--- src/Polynomial.ts
+++ src/Polynomial.ts
@@ -8,36 +8,36 @@ for (let exponent = 1, value = 1; exponent < 256; exponent++) {
     EXP[exponent % 255] = value;
 }
 
-function mul(a, b) {
+function mul(a: number, b: number): number {
     return a && b ? EXP[(LOG[a] + LOG[b]) % 255] : 0;
 }
 
-function div(a, b) {
+function div(a: number, b: number): number {
     return EXP[(LOG[a] + LOG[b] * 254) % 255];
 }
 
-function polyMul(poly1, poly2) {
-    const coeffs = new Uint8Array(poly1.length + poly2.length - 1);
+function polyMul(poly1: Uint8Array, poly2: Uint8Array): Uint8Array {
+    const coefficients = new Uint8Array(poly1.length + poly2.length - 1);
 
-    for (let index = 0; index < coeffs.length; index++) {
-        let coeff = 0;
-        for (let p1index = 0; p1index <= index; p1index++) {
-            const p2index = index - p1index;
-            coeff ^= mul(poly1[p1index], poly2[p2index]);
+    for (let index = 0, coefficient = 0; index < coefficients.length; index++) {
+        for (let p1Index = 0; p1Index <= index; p1Index++) {
+            const p2Index = index - p1Index;
+            coefficient ^= mul(poly1[p1Index], poly2[p2Index]);
         }
-        coeffs[index] = coeff;
+        coefficients[index] = coefficient;
     }
-    return coeffs;
+    return coefficients;
 }
 
-export function polyRest(dividend, divisor) {
+export function polyRest(dividend: Uint8Array, divisor: Uint8Array) {
     const quotientLength = dividend.length - divisor.length + 1;
     let rest = new Uint8Array(dividend);
+
     for (let count = 0; count < quotientLength; count++) {
         if (rest[0]) {
             const factor = div(rest[0], divisor[0]);
             const subtr = new Uint8Array(rest.length);
-            subtr.set(polyMul(divisor, [factor]), 0);
+            subtr.set(polyMul(divisor, new Uint8Array([factor])), 0);
             rest = rest.map((value, index) => value ^ subtr[index]).slice(1);
         } else {
             rest = rest.slice(1);
@@ -46,7 +46,7 @@ export function polyRest(dividend, divisor) {
     return rest;
 }
 
-export function getGeneratorPoly(degree) {
+export function getGeneratorPoly(degree: number): Uint8Array {
     let lastPoly = new Uint8Array([1]);
     for (let i = 0; i < degree; i++) {
         lastPoly = polyMul(lastPoly, new Uint8Array([1, EXP[i]]));
@@ -54,7 +54,7 @@ export function getGeneratorPoly(degree) {
     return lastPoly;
 }
 
-export function getEDC(data, codewords) {
+export function getEDC(data: number[], codewords: number) {
     const degree = codewords - data.length;
     const messagePoly = new Uint8Array(codewords);
     messagePoly.set(data, 0);
