c4517479cdd58bd3122dbf157c968d7c254906b0
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-08-14 16:57:50 +0100

    Add docs and remove unused code

--- src/CanvasGrid.ts
+++ src/CanvasGrid.ts
@@ -1,25 +1,21 @@
 /**
- *
+ * Controller class for the output canvas
  */
 export class CanvasGrid {
     canvasElement: HTMLCanvasElement;
-    cellColor: string;
     width: number;
     height: number;
-    cellArray: Array<string>;
     tileSize: number;
 
-    // canvasId: DOM Id for canvas
-    // size: Length in number of cells
+    /**
+     * @param canvasId DOM If of the canvas
+     * @param size How many cells wide the QR code is
+     */
     constructor(canvasId: string, size: number) {
         this.canvasElement = document.getElementById(canvasId) as HTMLCanvasElement;
 
-        this.cellColor = '#000000';
-
         this.width = size;
         this.height = size;
-
-        this.cellArray = [];
 
         this.canvasElement.width = Math.min(
             document.body.clientWidth / 2,
@@ -28,27 +24,15 @@ export class CanvasGrid {
         this.canvasElement.height = this.canvasElement.width;
 
         this.tileSize = this.canvasElement.width / size;
-
-        //this.canvas = new Canvas(canvasElement, Math.ceil(size));
     }
 
-    // Helper funcitons
-    findPosition(index: number) {
-        let x = index % this.width;
-        let y = Math.floor(index / this.width);
-        return [x, y];
-    }
-
-    findIndex(x: number, y: number) {
-        if (x >= this.width || y >= this.height || x < 0 || y < 0) {
-            return -1;
-        }
-        return this.width * y + x;
-    }
-
-    // Drawing functions
-    fillCell(n: number, color: string) {
-        let coords = this.findPosition(n);
+    /**
+     * Paint a cell with the given color
+     * @param index - Cell index
+     * @param color
+     */
+    fillCell(index: number, color: string) {
+        let coords = this._findPosition(index);
         let x = coords[0] * this.tileSize;
         let y = coords[1] * this.tileSize;
 
@@ -60,7 +44,16 @@ export class CanvasGrid {
         context.stroke();
     }
 
-    unfillCell(n: number) {
-        this.fillCell(n, '#000000');
+    _findPosition(index: number) {
+        let x = index % this.width;
+        let y = Math.floor(index / this.width);
+        return [x, y];
+    }
+
+    _findIndex(x: number, y: number) {
+        if (x >= this.width || y >= this.height || x < 0 || y < 0) {
+            return -1;
+        }
+        return this.width * y + x;
     }
 }
