84eae3f909158ed08b8851727c512c238b0148a8
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-08-14 11:00:43 +0100

    Use dist for example

--- Example/index.js
+++ Example/index.js
@@ -1,4 +1,4 @@
-import { QR } from '../EasyQR/QR.js';
+import { QR } from '/dist/src/index.js';
 
 window.addEventListener('load', function () {
     document.getElementById('submit').addEventListener('click', function () {
